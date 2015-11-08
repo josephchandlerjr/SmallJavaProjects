3d9c600adae617f16d6694a3004e9d0d834dec84
agnesi <josephalanchandlerjr@gmail.com>
2015-11-07 20:26:08 -0500

    add ArrayMethods

--- Udacity/IntroToJava/ArrayListMethods/ArrayListMethods.java
+++ Udacity/IntroToJava/ArrayListMethods/ArrayListMethods.java
@@ -32,12 +32,11 @@ public class ArrayListMethods
     {
         boolean sorted = true;
 	
-	for (int i = 0; i < list.size()-1; i++)
+	for (int i = 0; i < list.size()-1 && sorted; i++)
 	{
 		if (list.get(i).compareTo(list.get(i+1)) > 0)
 		{
 			sorted = false;
-			break;
 		}
 	}
 
